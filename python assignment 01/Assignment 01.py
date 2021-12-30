7ff6f66b67ee3fdf29b39de15fcad049448a0647
Saba Shehzad <sabashehzad505@gmail.com>
2021-12-31 04:00:08 +0500

    assignment complete

---
+++ python assignment 01/Assignment 01.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print('''Twinkle, twinle, little star,
+    How I wonder what you are!
+        Up above the world so high,
+        Like a diamond in the sky.
+Twinkle, Twinkle, little star,
+    How I wonder what you are!
+''')
+
+
+# In[2]:
+
+
+import sys
+print (sys.version)
+
+
+# In[3]:
+
+
+from datetime import datetime
+time = datetime.today()
+print(time)
+
+
+# In[5]:
+
+
+import math
+r=float(input("Enter the radius of circle"))
+area=math.pi*r*r
+print("the area of the circle with radius %.2f " %r +"is %.2f" %area)
+
+
+# In[12]:
+
+
+a=  input("Enter your first name: ")
+b= input("Enter your last name: ")
+print(b+" "+a)
+
+
+# In[19]:
+
+
+
+a=int(input("Enter your first number: "))
+b=int(input("Enter your last number: "))
+c=a+b
+print(c)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
